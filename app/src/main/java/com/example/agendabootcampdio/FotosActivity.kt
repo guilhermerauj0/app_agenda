0e1e99057a6fdef796a28cf3cb096a0dd038d8f3
arthu <arthurguilhermesb@gmail.com>
2022-02-07 12:26:29 -0300

    create permission and permission request to use camera

--- app/src/main/java/com/example/agendabootcampdio/FotosActivity.kt
+++ app/src/main/java/com/example/agendabootcampdio/FotosActivity.kt
@@ -10,6 +10,13 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_fotos.*
 
 class FotosActivity : AppCompatActivity() {
+
+    companion object{
+        private val PERMISSION_CODE_IMAGE_PICK = 1000
+        private val IMAGE_PICK_CODE = 1001
+        private  val PERMISSION_CODE_CAMERA_CAPTURE = 2000
+    } // USADO PARA GUARDAR CÓDIGOS DE PERMISSÃO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fotos)
@@ -20,7 +27,7 @@ class FotosActivity : AppCompatActivity() {
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
                         PackageManager.PERMISSION_DENIED) {
                     val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    requestPermissions(permission, PERMISSION_CODE)
+                    requestPermissions(permission, PERMISSION_CODE_IMAGE_PICK)
                 }else{
                     pickImageFromGallery()
                 }
@@ -30,6 +37,30 @@ class FotosActivity : AppCompatActivity() {
 
             }
         }
+
+        open_camera_button.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(checkSelfPermission(Manifest.permission.CAMERA) ==
+                        PackageManager.PERMISSION_DENIED ||
+                        checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                        PackageManager.PERMISSION_DENIED){
+
+                    val permissions = arrayOf(Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, PERMISSION_CODE_CAMERA_CAPTURE)
+
+
+                }else{
+                    openCamera()
+                }
+            }else{
+                openCamera()
+            }
+        }
+    }
+
+    private fun openCamera() {
+        TODO("Not yet implemented")
     }
 
     override fun onRequestPermissionsResult(
@@ -39,9 +70,20 @@ class FotosActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when(requestCode){
-            PERMISSION_CODE -> {
+            PERMISSION_CODE_IMAGE_PICK -> {
                 if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                     pickImageFromGallery()
+                }else{
+                    Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            PERMISSION_CODE_CAMERA_CAPTURE -> {
+                if((grantResults.size > 1) && // grantResults precisa ser 1 devido a ser 2 permissões pedidas
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    openCamera()
                 }else{
                     Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show()
                 }
@@ -63,9 +105,4 @@ class FotosActivity : AppCompatActivity() {
             image_view.setImageURI(data?.data)
         }
     }
-
-    companion object{
-        private val PERMISSION_CODE = 1000
-        private val IMAGE_PICK_CODE = 1001
-    } // USADO PARA GUARDAR CÓDIGOS DE PERMISSÃO
 }
